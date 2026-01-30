0f2f521b3a998256f51d14cf53bddb8b35423fea
lorespaul <47262696+lorespaul@users.noreply.github.com>
2026-01-30 20:24:14 +0100

    fix logging

--- main.py
+++ main.py
@@ -736,6 +736,5 @@ if __name__ == '__main__':
         app,
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 8080)),
-        log_config=log_config,
-        log_level="info",
+        log_config=log_config
     )
